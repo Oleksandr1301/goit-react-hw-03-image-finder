e256248700f965edb7e02bcf87153c972151a608
Oleksandr1301 <viss82@ukr.net>
2023-04-29 16:11:08 +0300

    add gallery

--- src/components/searchbar/searchbar.jsx
+++ src/components/searchbar/searchbar.jsx
@@ -1,15 +1,35 @@
-<header class="searchbar">
-  <form class="form">
-    <button type="submit" class="button">
-      <span class="button-label">Search</span>
-    </button>
+import { Component } from 'react';
+import PropTypes from "prop-types";
+import { Formik } from 'formik';
+import {
+  Form,
+  Field,
+  ButtonSearch,
+} from './searchbar.styled';
 
-    <input
-      class="input"
-      type="text"
-      autocomplete="off"
-      autofocus
-      placeholder="Search images and photos"
-    />
-  </form>
-</header>
+export class Searchbar extends Component {
+  render() {
+  return (
+    <Formik>
+      <Form  onSubmit={this.props.onSubmit}>
+        
+        <Field
+          
+          type="text"
+          name="search"
+          autoComplete="off"
+          autoFocus
+          placeholder="Search images and photos"
+        />
+        <ButtonSearch type="submit" >
+          <span >Search</span>
+        </ButtonSearch>
+      </Form>
+    </Formik>
+  );
+  }
+};
+
+Searchbar.propTypes = {
+  onSubmit: PropTypes.func,
+};
